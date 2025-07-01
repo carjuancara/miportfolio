c1fc385b2e6bde1b4ddb8f05f9f4078191ddd5ec
Carlos J Carabajal <carjuancara@gmail.com>
2025-07-01 16:44:11 -0300

    style: create title.svelte component

---
+++ src/lib/components/title/title.svelte
@@ -0,0 +1,25 @@
+<script>
+	const title = 'Events';
+</script>
+
+<section class="event">
+	<h2 class="event__title">{title}</h2>
+</section>
+
+<style>
+	.event {
+		display: flex;
+		width: 100%;
+		height: 10%;
+		padding: 4px 0 4px 4px;
+		align-items: center;
+		justify-content: flex-start;
+		color: white;
+		background-color: blueviolet;
+	}
+
+	.event__title {
+		font-size: 1rem;
+		font-weight: bold;
+	}
+</style>
