9fea4458d89dc3a677bfa80967623551a4260008
Carlos J Carabajal <carjuancara@gmail.com>
2025-07-02 10:57:00 -0300

    chrore: create description.svelte component

---
+++ src/lib/components/description/description.svelte
@@ -0,0 +1,26 @@
+<script>
+	const { description } = $props();
+</script>
+
+<section class="container">
+	<texttarea class="description">{description}</texttarea>
+</section>
+
+<style>
+	.container {
+		display: flex;
+		width: 100%;
+		height: 100%;
+	}
+	.description {
+		flex: 1;
+
+		border: none;
+		outline: none;
+		resize: none;
+		font-size: 1rem;
+		line-height: 1.5;
+		color: white;
+		background-color: dimgray;
+	}
+</style>
