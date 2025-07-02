eae77a1c7bdc4a3834f75ed04e8081312dff6018
Carlos J Carabajal <carjuancara@gmail.com>
2025-07-02 16:07:43 -0300

    feat: add responsive in button-bar and change class

--- src/lib/components/button-bar/buttonBar.svelte
+++ src/lib/components/button-bar/buttonBar.svelte
@@ -3,30 +3,32 @@
 </script>
 
 <nav class="container__nav">
-	<ul class="container__button__bar">
-		<li class="container__button__bar__item"><a href={demoUrl}>Demo</a></li>
-		<li class="container__button__bar__item"><a href={repoUrl}>Repo</a></li>
+	<ul class="container__button">
+		<li class="item"><a href={demoUrl}>Demo</a></li>
+		<li class="item"><a href={repoUrl}>Repo</a></li>
 	</ul>
 </nav>
 
 <style>
-	.container__nav {
-		background-color: #1e2939;
-		padding: 8px;
-		color: white;
-	}
-	.container__button__bar {
-		cursor: pointer;
-		border-radius: 8px;
-		gap: 8px;
-		padding-right: 8px;
-		display: flex;
-		justify-content: flex-end;
-	}
-	.container__button__bar__item {
-		cursor: pointer;
-		border-radius: 8px;
-		background-color: #155dfc;
-		padding: 8px;
+	@media (width <=768px) {
+		.container__nav {
+			background-color: #1e2939;
+			padding: 4px;
+			color: white;
+		}
+		.container__button {
+			cursor: pointer;
+			border-radius: 8px;
+			gap: 8px;
+			padding-right: 8px;
+			display: flex;
+			justify-content: flex-end;
+		}
+		.item {
+			cursor: pointer;
+			border-radius: 8px;
+			background-color: #155dfc;
+			padding: 8px;
+		}
 	}
 </style>
