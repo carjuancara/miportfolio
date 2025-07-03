e24c691862feb3c27ffcd807258fa4a27f8811e6
Carlos J Carabajal <carjuancara@gmail.com>
2025-07-03 09:43:37 -0300

    chore: create about component

---
+++ src/lib/components/about/about.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Title from '../title/title.svelte';
+
+	const { about } = $props();
+</script>
+
+<section class="container__about">
+	<section class="about__title">
+		<Title title="About" />
+	</section>
+	<p class="about__text">{about}</p>
+</section>
+
+<style>
+	@media (width <= 768px) {
+		.container__about {
+			padding: 1rem;
+			background-color: slateblue;
+		}
+		.about__title {
+			padding: 2rem 0;
+		}
+		.about__text {
+			color: white;
+			font-size: 1.2rem;
+		}
+	}
+</style>
