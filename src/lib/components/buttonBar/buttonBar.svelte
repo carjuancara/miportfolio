02cf4c5f14a12e2c3b1428e18eb72e07f0283a34
Carlos J Carabajal <carjuancara@gmail.com>
2025-07-05 17:56:51 -0300

    style: fixed styles

--- src/lib/components/buttonBar/buttonBar.svelte
+++ src/lib/components/buttonBar/buttonBar.svelte
@@ -4,34 +4,47 @@
 
 <nav class="container__nav">
 	<ul class="container__button">
-		<li class="item"><a href={demoUrl}>Demo</a></li>
-		<li class="item"><a href={repoUrl}>Repo</a></li>
+		<li class="item_container"><a class="item" href={demoUrl}>Demo</a></li>
+		<li class="item_container"><a class="item" href={repoUrl}>Repo</a></li>
 	</ul>
 </nav>
 
 <style>
-	@media (width <=768px) {
-		.container__nav {
-			background-color: slateblue;
-			padding: 4px;
-			color: white;
-		}
-		.container__button {
-			cursor: pointer;
-			border-radius: 8px;
-			gap: 8px;
-			padding-right: 8px;
-			display: flex;
-			justify-content: flex-end;
-		}
-		.item {
-			cursor: pointer;
-			border-radius: 8px;
-			background-color: white;
-			color: blueviolet;
-			font-size: 1.3rem;
-			font-weight: bold;
-			padding: 8px;
-		}
+	.container__nav {
+		padding: 4px;
+		color: var(--font-color-primary);
+	}
+
+	.container__button {
+		cursor: pointer;
+		border-radius: 8px;
+		gap: 16px;
+		padding-right: 1rem;
+		display: flex;
+		justify-content: flex-end;
+		align-items: center;
+	}
+
+	.item_container {
+		display: flex;
+		align-items: center;
+		margin-right: 4px;
+	}
+
+	.item {
+		padding: 8px;
+		border-radius: 8px;
+		flex-basis: 80px;
+		flex-shrink: 0;
+		background-color: var(--bg-color-tertiary);
+		color: var(--font-color-secondary);
+		font-size: 1.3rem;
+		text-align: center; /* Centra el texto dentro del item */
+	}
+
+	.item:hover {
+		cursor: pointer;
+		transform: scale(1.1);
+		transition: transform 0.3s ease-in-out;
 	}
 </style>
