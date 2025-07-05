150314d0da7ea9a5993df32e8ce76296708bb4aa
Carlos J Carabajal <carjuancara@gmail.com>
2025-07-05 18:00:38 -0300

    style: add media queries

--- src/lib/components/socialLink/socialLink.svelte
+++ src/lib/components/socialLink/socialLink.svelte
@@ -5,38 +5,81 @@
 <section class="banner">
 	<ul class="social-link">
 		{#each socialLink as social (social.name)}
-			<li><a class="banner__social" href={social.url}>{social.name}</a></li>
+			<li class="container__social">
+				<a class="banner__social" href={social.url}>{social.name}</a>
+			</li>
 		{/each}
 	</ul>
 </section>
 
 <style>
-	.banner {
-		width: 100%;
-		height: 100%;
-		color: white;
-	}
+	@media (width <= 768px) {
+		.banner {
+			width: 100%;
+			height: 100%;
+			color: var(--font-color-secondary);
+			padding-top: 16px;
+			background-color: var(--bg-color-secondary);
+		}
+		.social-link {
+			padding: 4px 0;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			gap: 4px;
+			font-size: 1.3rem;
+			background-color: var(--bg-color-secundary);
+		}
+		.container__social {
+			padding: 8px;
+		}
+		.banner__social {
+			place-content: center center;
+			padding: 8px;
+			border-radius: 8px;
+			place-items: center;
+			text-decoration: none;
 
-	.social-link {
-		padding: 4px 0;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		gap: 8px;
-		font-size: 1.3rem;
-		background-color: blueviolet;
-	}
-
-	.banner__social:hover {
-		color: blue;
-		cursor: pointer;
-		transform: scale(1.1);
-		transition: transform 0.3s ease-in-out;
+			color: var(--font-color-secundary);
+			background-color: var(--font-color-primary);
+		}
+		.banner__social:hover {
+			color: var(--font-color-secundary);
+			cursor: pointer;
+			transform: scale(1.1);
+			transition: transform 0.3s ease-in-out;
+		}
 	}
 
 	@media (width >768px) {
+		.banner {
+			width: 100%;
+			height: 100%;
+			color: var(--font-color-primary);
+		}
 		.social-link {
-			border-radius: 0;
+			padding: 4px 0;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			gap: 4px;
+			font-size: 1.3rem;
+			background-color: transparent;
+		}
+		.banner__social {
+			place-content: center center;
+			padding: 8px;
+			border-radius: 8px;
+			place-items: center;
+			text-decoration: none;
+
+			color: var(--font-color-secondary);
+			background-color: var(--bg-color-tertiary);
+		}
+		.banner__social:hover {
+			cursor: pointer;
+			transform: scale(1.1);
+			transition: transform 0.3s ease-in-out;
 		}
 	}
 </style>
