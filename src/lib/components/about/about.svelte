4ad230480d6843fa2f63b4d3fbff44554a9ba8df
Carlos J Carabajal <carjuancara@gmail.com>
2025-07-05 17:52:51 -0300

    style: add @media query 768px

--- src/lib/components/about/about.svelte
+++ src/lib/components/about/about.svelte
@@ -14,14 +14,27 @@
 	@media (width <= 768px) {
 		.container__about {
 			padding: 1rem;
-			background-color: slateblue;
+			background-color: var(--bg-color-secondary);
 		}
 		.about__title {
 			padding: 2rem 0;
 		}
 		.about__text {
-			color: white;
+			color: var(--font-color-primary);
 			font-size: 1.2rem;
+		}
+	}
+	@media (width > 768px) {
+		.container__about {
+			padding: 1rem;
+			background-color: var(--bg-color-secondary);
+			height: 100%;
+		}
+		.about__text {
+			padding: 1rem 0.8rem;
+			color: var(--font-color-primary);
+			font-size: 1.1rem;
+			overflow-y: auto;
 		}
 	}
 </style>
