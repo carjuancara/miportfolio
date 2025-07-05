af365a5b40e08b43ee46b095d8ec0d3b08124ed5
Carlos J Carabajal <carjuancara@gmail.com>
2025-07-05 17:57:46 -0300

    styles: add media query 768px

--- src/lib/components/card/card.svelte
+++ src/lib/components/card/card.svelte
@@ -13,11 +13,13 @@
 	<div class="container__image">
 		<ViewImage {images} />
 	</div>
-	<div class="container__description">
-		<Description {description} />
-	</div>
-	<div class="container__button-bar">
-		<ButtonBar {demoUrl} {repoUrl} />
+	<div class="container__bottom">
+		<div class="container__description">
+			<Description {description} />
+		</div>
+		<div class="container__button-bar">
+			<ButtonBar {demoUrl} {repoUrl} />
+		</div>
 	</div>
 </section>
 
@@ -29,10 +31,46 @@
 			align-items: center;
 			padding: 4px;
 			box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-			background-color: slateblue;
+			background-color: var(--bg-color-secondary);
 		}
 		.container__title {
 			margin-bottom: 8px;
+			display: flex;
+			width: 100%;
+		}
+		.container__image {
+			display: flex;
+			width: 100%;
+			height: 202px;
+			margin-bottom: 8px;
+		}
+		.container__description {
+			display: flex;
+			width: 100%;
+			height: 100px;
+			margin-bottom: 8px;
+		}
+		.container__button-bar {
+			display: flex;
+			justify-content: flex-end;
+			width: 100%;
+			margin-bottom: 1px;
+		}
+	}
+	@media (width > 768px) {
+		.card {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			border: 2px solid var(--color-tertiary);
+			border-radius: 8px;
+			padding: 4px;
+			box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+		}
+		.container__bottom {
+			background-color: var(--bg-color-secondary);
+		}
+		.container__title {
 			display: flex;
 			width: 100%;
 		}
