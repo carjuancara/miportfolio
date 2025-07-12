a2d083e6f05bfe3caffa4ae4f9588bf6286e97b2
Carlos J Carabajal <carjuancara@gmail.com>
2025-07-12 10:38:02 -0300

    fix: fixed string

--- src/lib/components/imageViewer/viewImage.svelte
+++ src/lib/components/imageViewer/viewImage.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <section class="container-image">
-	<img class="img__image" src={base / image || '#'} alt={base / image} />
+	<img class="img__image" src={base + image || '#'} alt={base + image} />
 </section>
 
 <style>
