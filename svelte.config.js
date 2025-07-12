3dc1803c64fe5c9f12502ef6536ba6e2d639ddfb
Carlos J Carabajal <carjuancara@gmail.com>
2025-07-12 09:05:47 -0300

    chore: update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.NODE_ENV === 'production' ? '/miportfolio' : ''
 		}
 	}
 };
